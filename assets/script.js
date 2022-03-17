e1142ba70cf8aa6bbf095ec36ec52e61cf1a4ded
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-03-17 16:04:18 -0700

    add cdn and start script.js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var questionsArray = [
+{
+    question:"Javascript is an ________ language?",
+    choices: ["object-oriented", "object-based", "Procedural", "None of the above"],
+    answer:"object-oriented"
+},
+{
+    question:"",
+    choices: [],
+    answer:""
+},
+{
+    question:"",
+    choices: [],
+    answer:""
+},
+{
+    question:"",
+    choices: [],
+    answer:""
+},
+{
+    question:"",
+    choices: [],
+    answer:""
+},
+]
+
+quizBtn.addEventListener("click". generateQuestion);
