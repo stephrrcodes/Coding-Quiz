5ed6c8c3e356e5d27e473bb3712ee3c231c5332a
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-03-20 15:49:35 -1000

    fixed start button

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,58 @@
-var questionsArray = [
-{
-    question:"Javascript is an ________ language?",
-    choices: ["object-oriented", "object-based", "Procedural", "None of the above"],
-    answer:"object-oriented"
-},
-{
-    question:"",
-    choices: [],
-    answer:""
-},
-{
-    question:"",
-    choices: [],
-    answer:""
-},
-{
-    question:"",
-    choices: [],
-    answer:""
-},
-{
-    question:"",
-    choices: [],
-    answer:""
-},
+var questionsInfo = [
+    {
+        question:"Javascript is an ________ language?",
+        choices: ["object-oriented", "object-based", "Procedural", "None of the above"],
+        answer:"object-oriented"
+    },
+    {
+        question:"",
+        choices: [],
+        answer:""
+    },
+    {
+        question:"",
+        choices: [],
+        answer:""
+    },
+    {
+        question:"",
+        choices: [],
+        answer:""
+    },
+    {
+        question:"",
+        choices: [],
+        answer:""
+    },
 ]
+var question= document.getElementById("question")
+var choices = document.querySelectorAll("choice-btn") 
+var quizBtn = document.getElementById("quiz-btn");
+var qContainer = document.querySelector("#q-container")
+var countDown = document.querySelector("#countdown")
+countDown.textContent = quizTime;
+var quizTime = 80
 
-quizBtn.addEventListener("click". generateQuestion);
+console.log(choices)
+
+var currentQuestionIndex = 0;
+function startQuiz() {
+    quizTime = setInterval(startTimer, 1000);
+    loadQuestion();
+}
+
+function startTimer(){
+        quizTime--;
+        countDown.textContent = quizTime;
+}
+
+function loadQuestion(){
+    console.log ("question click");
+    question.textContent = questionsInfo[currentQuestionIndex].question;
+        for (
+            var i= 0;
+            i < questionsInfo[currentQuestionIndex].choices.length;) i++;    
+    choices.textContent = questionsInfo[currentQuestionIndex].choices[i];
+}
+
+quizBtn.addEventListener("click",loadQuestion);
